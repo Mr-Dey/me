d4598ac69e855510765aa40bb933832ef1c6756e
rahul-666 <rahuldasbucha619@gmail.com>
2023-11-25 19:00:42 +0530

    updated

---
+++ logic.js
@@ -0,0 +1,10 @@
+// let mail=document.getElementById("mail");
+
+
+// mail.addEventListener("mouseover",()=>{
+//     mail.style.backgroundColor="#ff7613";
+//     mail.style.color="white";
+//     mail.style.fontSize="1.6rem";
+//     mail.style.borderRadius="8px";
+//     mail.style.padding="2px";
+// })
