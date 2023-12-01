75b4e7e72a02aa507b26eb79ff682792ef58eee8
Mr-Dey <rahuldasbucha619@gmail.com>
2023-12-01 18:36:28 +0530

    navbar change logs: added blur effect, added animation

--- logic.js
+++ logic.js
@@ -1,26 +1,37 @@
-const navBar =document.querySelector(".nav");
+const nav =document.querySelector(".nav");
 const navButton=document.querySelector("#navButton");
 const navList=document.querySelector(".navList");
+const navListItems=document.querySelector(".navListItems");
 
-let showNavbar=true;
+let showNavbar=false;
 
-navList.style.transition="all 0.5s ease";
-navList.style.height="240px";
-navList.style.overflowX="auto";
-// navList.style.backdropFilter="blur(6px)";
+navList.style.transition="all 1s ease, height 0.3s ease, opacity 0.3s ease";
+navList.style.opacity="0";
+navList.style.height="0px";
+navList.style.width="10%";
+navList.style.margin="0px auto";
+navList.style.fontSize="0rem";
 
 
 
 navButton.addEventListener('click',()=>{
     if(showNavbar==true){
         showNavbar=false;
+
+        navList.style.opacity="0";
         navList.style.height="0px";
-        // navList.style.display="none";
+        navList.style.width="20%";
+        navList.style.margin="0px auto";
+        navList.style.fontSize="0rem";
     }
     else{
         showNavbar=true;
+
+        navList.style.opacity="1";
         navList.style.height="240px";
-        // navList.style.display="block";
+        navList.style.width="80%";
+        navList.style.margin="30px auto";
+        navList.style.fontSize="1.5rem";
     }
     console.log(showNavbar);
 })
