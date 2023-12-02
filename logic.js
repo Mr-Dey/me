fcf6598807251482c1b8f222799004c6feff8a21
Mr-Dey <rahuldasbucha619@gmail.com>
2023-12-02 16:04:20 +0530

    updated navbar animation

--- logic.js
+++ logic.js
@@ -5,6 +5,7 @@ const navListItems=document.querySelector(".navListItems");
 
 let showNavbar=false;
 nav.style.transition="none";
+navList.style.transition="none";
 navList.style.transition="all 1s ease, height 0.3s ease, opacity 0.3s ease";
 navList.style.opacity="0";
 navList.style.height="0px";
@@ -25,16 +26,18 @@ navButton.addEventListener('click',()=>{
         navList.style.margin="0px auto";
         navList.style.fontSize="0rem";
         navList.style.padding="0px";
+        navButton.classList.remove("navbutton_clicked")
         
     }
     else{
         showNavbar=true;
         navList.style.opacity="1";
         navList.style.height="240px";
-        navList.style.width="80%";
-        navList.style.margin="30px auto";
+        navList.style.width="70%";
+        navList.style.margin="10px auto";
         navList.style.fontSize="1.5rem";
         navList.style.padding="5px";
+        navButton.classList.add("navbutton_clicked")
     }
     console.log(showNavbar);
 })
