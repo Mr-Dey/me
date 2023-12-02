269ef6842b8a2db36d405d9fe5b1fb1350d573ed
Mr-Dey <rahuldasbucha619@gmail.com>
2023-12-02 14:03:34 +0530

    beta update

--- logic.js
+++ logic.js
@@ -4,13 +4,14 @@ const navList=document.querySelector(".navList");
 const navListItems=document.querySelector(".navListItems");
 
 let showNavbar=false;
-
+nav.style.transition="none";
 navList.style.transition="all 1s ease, height 0.3s ease, opacity 0.3s ease";
 navList.style.opacity="0";
 navList.style.height="0px";
 navList.style.width="10%";
 navList.style.margin="0px auto";
 navList.style.fontSize="0rem";
+navList.style.padding="0px";
 
 
 
@@ -23,15 +24,17 @@ navButton.addEventListener('click',()=>{
         navList.style.width="20%";
         navList.style.margin="0px auto";
         navList.style.fontSize="0rem";
+        navList.style.padding="0px";
+        
     }
     else{
         showNavbar=true;
-
         navList.style.opacity="1";
         navList.style.height="240px";
         navList.style.width="80%";
         navList.style.margin="30px auto";
         navList.style.fontSize="1.5rem";
+        navList.style.padding="5px";
     }
     console.log(showNavbar);
 })
